9fbf4d3c9dc179f9c5e8c88de8dfe6b304a07142
Jarod Kober <jarod.development@gmail.com>
2023-08-28 16:01:12 -0700

    PrimeReact Installation * Installed the PrimeReact UI component library * Imported the Bootstrap dark blue theme * Wrapped the app in the PrimeReactProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+import { PrimeReactProvider } from 'primereact/api';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PrimeReactProvider>
+				<App />
+			</PrimeReactProvider>
 		</Provider>
 	</React.StrictMode>
 );
