d888dd2c487bf9c4e1cdfffdb50f9e42dcb522ad
Jarod Kober <jarod.development@gmail.com>
2023-08-30 10:46:51 -0700

    Imported PrimeReact Core Styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import 'primeicons/primeicons.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 import { PrimeReactProvider } from 'primereact/api';
 import App from './App.jsx';
 
