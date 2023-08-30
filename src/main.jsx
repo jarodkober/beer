acd555d47b2a7805f68e30fe24493ab919ab9607
Jarod Kober <jarod.development@gmail.com>
2023-08-30 09:43:01 -0700

    Default Font Family * Set the default font family to use the variable defined by PrimeReact to ensure a unified look between PrimeReact components and custom components

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/global.scss';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
