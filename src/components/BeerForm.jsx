9eceef2de3be760ac0e376b18675ec1ba9c8aad5
Jarod Kober <jarod.development@gmail.com>
2023-08-31 14:46:22 -0700

    Moved BeerForm into React Hook Form * Converted the BeerForm component into React Hook Form * Defined some validation rules and error messages

--- src/components/BeerForm.jsx
+++ src/components/BeerForm.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useAddBeerMutation } from '../store';
 import { Button } from 'primereact/button';
 import { InputNumber } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
 import { PropTypes } from 'prop-types';
+import { Controller, useForm } from 'react-hook-form';
+import classNames from 'classnames';
 
 function BeerForm({ onHide }) {
 	BeerForm.propTypes = {
 		onHide: PropTypes.func
 	};
 
-	const [state, setState] = useState({
+	const defaultValues = {
 		abv: '',
 		brewery: '',
 		location: '',
@@ -20,18 +22,34 @@ function BeerForm({ onHide }) {
 		stylePrimary: '',
 		styleSecondary: '',
 		vintage: ''
-	});
+	};
+
+	const {
+		control,
+		formState: { errors },
+		handleSubmit,
+		reset,
+		watch
+	} = useForm({ defaultValues });
+
+	console.log(watch('name'));
+
+	console.log(watch('vintage'));
 
 	const [addBeer, results] = useAddBeerMutation();
 
-	const handleFormSubmit = (event) => {
-		event.preventDefault();
+	const onSubmit = (data) => {
+		addBeer(data);
 
-		addBeer(state);
+		reset();
 	};
 
-	const handleChange = (event) => {
-		setState({ ...state, [event.target.id]: event.target.value });
+	const getFormErrorMessage = (name) => {
+		return (
+			errors[name] && (
+				<small className="p-error">{errors[name].message}</small>
+			)
+		);
 	};
 
 	useEffect(() => {
@@ -41,93 +59,303 @@ function BeerForm({ onHide }) {
 	}, [onHide, results]);
 
 	return (
-		<form onSubmit={handleFormSubmit}>
-			<span className="p-float-label">
-				<InputText
-					id="name"
-					onChange={handleChange}
-					value={state.name}
-				/>
-				<label htmlFor="name">Beer Name</label>
-			</span>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="name"
+						control={control}
+						rules={{
+							required: 'A beer name is required.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputText
+									autoFocus
+									className={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									id={field.name}
+									{...field}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="name"
+					>
+						Beer Name
+					</label>
+				</span>
 
-			<span className="p-float-label">
-				<InputNumber
-					id="vintage"
-					min={1900}
-					onValueChange={handleChange}
-					value={state.vintage}
-				/>
-				<label htmlFor="vintage">Vintage</label>
-			</span>
+				{getFormErrorMessage('name')}
+			</div>
 
-			<span className="p-float-label">
-				<InputText
-					id="brewery"
-					onChange={handleChange}
-					value={state.brewery}
-				/>
-				<label htmlFor="brewery">Brewery</label>
-			</span>
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="vintage"
+						control={control}
+						rules={{
+							validate: (value) =>
+								(value >= 1900 &&
+									value <= new Date().getFullYear() + 1) ||
+								'Enter a valid year.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputNumber
+									id={field.name}
+									inputClassName={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									maxLength={4}
+									onBlur={field.onBlur}
+									onValueChange={(e) => field.onChange(e)}
+									ref={field.ref}
+									useGrouping={false}
+									value={field.value}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="vintage"
+					>
+						Vintage Year
+					</label>
+				</span>
 
-			<span className="p-float-label">
-				<InputText
-					id="stylePrimary"
-					onChange={handleChange}
-					value={state.stylePrimary}
-				/>
-				<label htmlFor="stylePrimary">Style (Primary)</label>
-			</span>
+				{getFormErrorMessage('vintage')}
+			</div>
 
-			<span className="p-float-label">
-				<InputText
-					id="styleSecondary"
-					onChange={handleChange}
-					value={state.styleSecondary}
-				/>
-				<label htmlFor="styleSecondary">Style (Secondary)</label>
-			</span>
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="brewery"
+						control={control}
+						rules={{
+							required: 'A brewery name is required.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputText
+									className={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									id={field.name}
+									{...field}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="brewery"
+					>
+						Brewery
+					</label>
+				</span>
 
-			<span className="p-float-label">
-				<InputText
-					id="location"
-					onChange={handleChange}
-					value={state.location}
-				/>
-				<label htmlFor="location">Location</label>
-			</span>
+				{getFormErrorMessage('brewery')}
+			</div>
 
-			<span className="p-float-label">
-				<InputNumber
-					id="abv"
-					maxFractionDigits={2}
-					minFractionDigits={2}
-					onValueChange={handleChange}
-					suffix="%"
-					value={state.abv}
-				/>
-				<label htmlFor="abv">ABV</label>
-			</span>
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="stylePrimary"
+						control={control}
+						rules={{
+							required: 'A primary beer style is required.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputText
+									className={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									id={field.name}
+									{...field}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="stylePrimary"
+					>
+						Style (Primary)
+					</label>
+				</span>
 
-			<span className="p-float-label">
-				<InputNumber
-					id="size"
-					onValueChange={handleChange}
-					suffix=" ml"
-					value={state.size}
-				/>
-				<label htmlFor="size">Size (ml)</label>
-			</span>
+				{getFormErrorMessage('stylePrimary')}
+			</div>
 
-			<span className="p-float-label">
-				<InputNumber
-					id="quantity"
-					min={1}
-					onValueChange={handleChange}
-					value={state.quantity}
-				/>
-				<label htmlFor="quantity">Quantity</label>
-			</span>
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="styleSecondary"
+						control={control}
+						rules={{
+							required: 'A secondary beer style is required.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputText
+									className={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									id={field.name}
+									{...field}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="styleSecondary"
+					>
+						Style (Secondary)
+					</label>
+				</span>
+
+				{getFormErrorMessage('styleSecondary')}
+			</div>
+
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="location"
+						control={control}
+						rules={{
+							required: 'A beer cellar location is required.'
+						}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputText
+									className={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									id={field.name}
+									{...field}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="location"
+					>
+						Location
+					</label>
+				</span>
+
+				{getFormErrorMessage('location')}
+			</div>
+
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="abv"
+						control={control}
+						rules={{}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputNumber
+									id={field.name}
+									inputClassName={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									maxFractionDigits={2}
+									minFractionDigits={2}
+									onBlur={field.onBlur}
+									onValueChange={(e) => field.onChange(e)}
+									ref={field.ref}
+									suffix="%"
+									useGrouping={false}
+									value={field.value}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="abv"
+					>
+						ABV
+					</label>
+				</span>
+
+				{getFormErrorMessage('abv')}
+			</div>
+
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="size"
+						control={control}
+						rules={{}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputNumber
+									id={field.name}
+									inputClassName={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									onBlur={field.onBlur}
+									onValueChange={(e) => field.onChange(e)}
+									ref={field.ref}
+									suffix=" ml"
+									value={field.value}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="size"
+					>
+						Size
+					</label>
+				</span>
+
+				{getFormErrorMessage('size')}
+			</div>
+
+			<div className="field">
+				<span className="p-float-label">
+					<Controller
+						name="quantity"
+						control={control}
+						rules={{}}
+						render={({ field, fieldState }) => (
+							<>
+								<InputNumber
+									id={field.name}
+									inputClassName={classNames({
+										'p-invalid': fieldState.invalid
+									})}
+									min={1}
+									onBlur={field.onBlur}
+									onValueChange={(e) => field.onChange(e)}
+									ref={field.ref}
+									value={field.value}
+								/>
+							</>
+						)}
+					/>
+					<label
+						className={classNames({ 'p-error': errors.name })}
+						htmlFor="quantity"
+					>
+						Quantity
+					</label>
+				</span>
+
+				{getFormErrorMessage('quantity')}
+			</div>
 
 			<Button
 				icon="pi pi-plus"
