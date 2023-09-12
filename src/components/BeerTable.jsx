2ffefc9f5fe9085e45f9a9b41c6f315e34b26281
Jarod Kober <jarod.development@gmail.com>
2023-09-12 12:12:47 -0700

    BeerTable Filter * Added the ability to filter the BeerTable by name, vintage, brewery, style, and cellar

--- src/components/BeerTable.jsx
+++ src/components/BeerTable.jsx
@@ -17,32 +17,47 @@ function BeerTable() {
 	return (
 		<section>
 			<DataTable
+				filterDisplay="row"
 				sortMode="multiple"
 				stripedRows
 				value={content}
 			>
 				<Column
 					field="beer_name"
+					filter
+					filterMatchMode="contains"
+					filterPlaceholder="Filter by Beer"
 					header="Beer Name"
 					sortable
 				></Column>
 				<Column
 					field="beer_vintage"
+					filter
+					filterPlaceholder="Filter by Vintage"
 					header="Vintage"
 					sortable
 				></Column>
 				<Column
 					field="brewery_name"
+					filter
+					filterMatchMode="contains"
+					filterPlaceholder="Filter by Brewery"
 					header="Brewery"
 					sortable
 				></Column>
 				<Column
 					field="beer_style"
+					filter
+					filterMatchMode="contains"
+					filterPlaceholder="Filter by Style"
 					header="Style"
 					sortable
 				></Column>
 				<Column
 					field="cellar_name"
+					filter
+					filterMatchMode="contains"
+					filterPlaceholder="Filter by Cellar"
 					header="Cellar"
 					sortable
 				></Column>
