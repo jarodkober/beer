3827d56039faa2bc7fcfc15d8fc230d4877f7682
Jarod Kober <jarod.development@gmail.com>
2023-09-14 15:57:07 -0700

    Authentication * Implemented user authentication with AWS Amplify and Cognito * Added very basic initial header styles including a Sign Out button with the user email * Updated index.html with a hacky fix for the script error caused by the combination of Vite & AWS CLI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import BeerButton from './components/beerButton/BeerButton';
 import BeerTable from './components/beerTable/BeerTable';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 
+Amplify.configure(awsExports);
+
 function App() {
 	return (
-		<>
-			<Header />
-			<main>
-				<BeerButton />
-				<BeerTable />
-			</main>
-			<Footer />
-		</>
+		<Authenticator>
+			{({ signOut, user }) => (
+				<>
+					<Header
+						signOut={signOut}
+						user={user}
+					/>
+					<main>
+						<BeerButton />
+						<BeerTable />
+					</main>
+					<Footer />
+				</>
+			)}
+		</Authenticator>
 	);
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
-function Header() {
+import styles from './Header.module.scss';
+import { PropTypes } from 'prop-types';
+import { Button } from 'primereact/button';
+
+function Header({ signOut, user }) {
+	Header.propTypes = {
+		signOut: PropTypes.func,
+		user: PropTypes.object
+	};
+
 	return (
-		<header>
+		<header className={styles.header}>
 			<h1>Beer Cellar Management</h1>
+			<Button onClick={signOut}>
+				Sign Out <br />
+				{user.attributes.email}
+			</Button>
 		</header>
 	);
 }
