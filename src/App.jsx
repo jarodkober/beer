f3768c056c531912d68fb080d5ba4b524a0f89a0
Jarod Kober <jarod.development@gmail.com>
2023-09-20 14:11:06 -0700

    Authenticator Styling * Converted Authenticator to modal variation * Applied dark mode styles, which are more closely aligned with the overall app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-import { Authenticator } from '@aws-amplify/ui-react';
+import {
+	Authenticator,
+	defaultDarkModeOverride,
+	ThemeProvider
+} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import BeerButton from './components/beerButton/BeerButton';
 import BeerTable from './components/beerTable/BeerTable';
@@ -10,22 +14,32 @@ import Header from './components/header/Header';
 Amplify.configure(awsExports);
 
 function App() {
+	const theme = {
+		name: 'my-theme',
+		overrides: [defaultDarkModeOverride]
+	};
+
 	return (
-		<Authenticator>
-			{({ signOut, user }) => (
-				<>
-					<Header
-						signOut={signOut}
-						user={user}
-					/>
-					<main>
-						<BeerButton />
-						<BeerTable />
-					</main>
-					<Footer />
-				</>
-			)}
-		</Authenticator>
+		<ThemeProvider
+			colorMode="dark"
+			theme={theme}
+		>
+			<Authenticator variation="modal">
+				{({ signOut, user }) => (
+					<>
+						<Header
+							signOut={signOut}
+							user={user}
+						/>
+						<main>
+							<BeerButton />
+							<BeerTable />
+						</main>
+						<Footer />
+					</>
+				)}
+			</Authenticator>
+		</ThemeProvider>
 	);
 }
 
