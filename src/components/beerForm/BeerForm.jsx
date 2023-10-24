54af4d44892303d2b25c303a32188eaf55714181
Jarod Kober <jarod.development@gmail.com>
2023-10-24 12:26:32 -0700

    Optimized Quantity Dropdown * Implemented a more elegant solution for generating the quantity dropdown * Changed the max quantity to 24 to match a case of some beers

--- src/components/beerForm/BeerForm.jsx
+++ src/components/beerForm/BeerForm.jsx
@@ -56,20 +56,9 @@ function BeerForm({ onHide, toast, user }) {
 		);
 	};
 
-	const quantities = [
-		{ label: '1', value: 1 },
-		{ label: '2', value: 2 },
-		{ label: '3', value: 3 },
-		{ label: '4', value: 4 },
-		{ label: '5', value: 5 },
-		{ label: '6', value: 6 },
-		{ label: '7', value: 7 },
-		{ label: '8', value: 8 },
-		{ label: '9', value: 9 },
-		{ label: '10', value: 10 },
-		{ label: '11', value: 11 },
-		{ label: '12', value: 12 }
-	];
+	const quantities = [...Array(24)]
+		.map((_, i) => i + 1)
+		.map((i) => ({ label: i, value: i }));
 
 	// TODO: Shift the list of beer styles into a data file
 	const beer_styles = [
