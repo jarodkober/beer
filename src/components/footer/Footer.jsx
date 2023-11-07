7a673a4879a376bedc1f6df07a03d5dd68191c36
Jarod Kober <jarod.development@gmail.com>
2023-11-07 11:36:18 -0700

    Footer Cleanup * Removed placeholder copy from the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import styles from './Footer.module.scss';
 
 function Footer() {
-	return <footer className={styles.footer}>Footer stuff</footer>;
+	return <footer className={styles.footer}></footer>;
 }
 
 export default Footer;
