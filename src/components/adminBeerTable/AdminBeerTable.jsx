186008fa065b89d6ae8fecdcd77b1140177c0fbb
Jarod Kober <jarod.development@gmail.com>
2024-01-09 13:28:32 -0700

    Update Beer Feature v.2 * Added the AdminBeerTable component that should have been included with the previous commit

--- src/components/adminBeerTable/AdminBeerTable.jsx
+++ src/components/adminBeerTable/AdminBeerTable.jsx
@@ -1,8 +1,15 @@
 import { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
-import { useGetBeersQuery } from '../../store';
+import {
+	useGetBeersQuery,
+	useGetBreweriesQuery,
+	useGetStylesQuery,
+	useUpdateBeerMutation
+} from '../../store';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
 import { Skeleton } from 'primereact/skeleton';
 import AdminBeerTableDeleteButton from '../adminBeerTableDeleteButton/AdminBeerTableDeleteButton';
 
@@ -13,6 +20,18 @@ function AdminBeerTable({ toast, user }) {
 	};
 
 	const { data, error, isLoading } = useGetBeersQuery({
+		user_auth: user.signInUserSession.idToken.jwtToken
+	});
+
+	const [updateBeer, results] = useUpdateBeerMutation();
+
+	const { data: breweriesData, error: breweriesError } = useGetBreweriesQuery(
+		{
+			user_auth: user.signInUserSession.idToken.jwtToken
+		}
+	);
+
+	const { data: stylesData, error: stylesError } = useGetStylesQuery({
 		user_auth: user.signInUserSession.idToken.jwtToken
 	});
 
@@ -39,6 +58,55 @@ function AdminBeerTable({ toast, user }) {
 		);
 	};
 
+	const onRowEditComplete = (e) => {
+		updateBeer({
+			beer_abv: e.newData.beer_abv,
+			beer_id: e.data.beer_id,
+			beer_name: e.newData.beer_name,
+			beer_style_id: e.newData.beer_style_id,
+			beer_verified: e.newData.beer_verified,
+			beer_vintage: e.newData.beer_vintage,
+			brewery_id: e.newData.brewery_id,
+			user_auth: user.signInUserSession.idToken.jwtToken,
+			user_id: user.username
+		});
+	};
+
+	const breweryEditor = (options) => {
+		return (
+			<Dropdown
+				onChange={(e) => options.editorCallback(e.value)}
+				optionLabel="brewery_name"
+				optionValue="brewery_id"
+				options={breweriesData}
+				value={options.rowData.brewery_id}
+			/>
+		);
+	};
+
+	const stylesEditor = (options) => {
+		return (
+			<Dropdown
+				onChange={(e) => options.editorCallback(e.value)}
+				optionLabel="beer_style"
+				optionValue="beer_style_id"
+				options={stylesData}
+				value={options.rowData.beer_style_id}
+			/>
+		);
+	};
+
+	const textEditor = (options) => {
+		return (
+			<InputText
+				onChange={(e) => options.editorCallback(e.target.value)}
+				required={true}
+				type="text"
+				value={options.value}
+			/>
+		);
+	};
+
 	useEffect(() => {
 		error &&
 			toast.current.show({
@@ -49,11 +117,43 @@ function AdminBeerTable({ toast, user }) {
 			});
 	}, [error, toast]);
 
+	useEffect(() => {
+		results.error &&
+			toast.current.show({
+				detail: 'An error occurred while editing the beer. This is most likely due to a cost-saving measure that pauses the database during periods of inactivity. Please try again in 30 seconds.',
+				severity: 'error',
+				sticky: true,
+				summary: 'Error'
+			});
+	}, [results.error, toast]);
+
+	useEffect(() => {
+		breweriesError &&
+			toast.current.show({
+				detail: 'An error occurred while loading the breweries. This is most likely due to a cost-saving measure that pauses the database during periods of inactivity. Please try again in 30 seconds.',
+				severity: 'error',
+				sticky: true,
+				summary: 'Error'
+			});
+	});
+
+	useEffect(() => {
+		stylesError &&
+			toast.current.show({
+				detail: 'An error occurred while loading the beer styles. This is most likely due to a cost-saving measure that pauses the database during periods of inactivity. Please try again in 30 seconds.',
+				severity: 'error',
+				sticky: true,
+				summary: 'Error'
+			});
+	});
+
 	return (
 		<section>
 			<DataTable
+				editMode="row"
 				filterDisplay="row"
 				header={header}
+				onRowEditComplete={onRowEditComplete}
 				scrollable
 				scrollHeight="flex"
 				sortMode="multiple"
@@ -63,6 +163,7 @@ function AdminBeerTable({ toast, user }) {
 			>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => textEditor(options)}
 					field="beer_name"
 					filter
 					filterMatchMode="contains"
@@ -72,6 +173,7 @@ function AdminBeerTable({ toast, user }) {
 				/>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => breweryEditor(options)}
 					field="brewery_name"
 					filter
 					filterMatchMode="contains"
@@ -81,6 +183,7 @@ function AdminBeerTable({ toast, user }) {
 				/>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => textEditor(options)}
 					field="beer_vintage"
 					filter
 					filterPlaceholder="Filter by Vintage"
@@ -89,6 +192,7 @@ function AdminBeerTable({ toast, user }) {
 				/>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => stylesEditor(options)}
 					field="beer_style"
 					filter
 					filterMatchMode="contains"
@@ -98,16 +202,23 @@ function AdminBeerTable({ toast, user }) {
 				/>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => textEditor(options)}
 					field="beer_abv"
 					header="ABV"
 					sortable
 				/>
 				<Column
 					body={isLoading && skeletonContent}
+					editor={(options) => textEditor(options)}
 					field="beer_verified"
 					header="Verified"
 					sortable
 				/>
+				<Column
+					bodyStyle={{ textAlign: 'center' }}
+					headerStyle={{ width: '2%' }}
+					rowEditor
+				></Column>
 				<Column
 					body={deleteTemplate}
 					bodyStyle={{ textAlign: 'center' }}
